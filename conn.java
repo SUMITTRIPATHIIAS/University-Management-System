72dbb40555786d7b5c8ede58ef955768d2b83785
SUMITTRIPATHIIAS <51814715+SUMITTRIPATHIIAS@users.noreply.github.com>
2021-04-08 16:09:33 +0530

    Add files via upload

---
+++ conn.java
@@ -0,0 +1,30 @@
+//package university.managment.system;
+package university.managment.system;
+
+
+/**
+ *
+ * @author DELL
+ */
+//public class conn {
+//package university.management.system;
+
+import java.sql.*;  
+
+public class conn{
+    Connection c;
+    Statement s;
+    public conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///ums","root","");    
+            s =c.createStatement();  
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
+    
+
